4cb101d45cb3ac5be8adb8d5596983aac394eaca
Brian Hogan <brianhogan@napcs.com>
2009-04-04 14:43:59 -0500

    little cleanup

--- simple/keys.rb
+++ simple/keys.rb
@@ -5,15 +5,13 @@ Shoes.app :width => 200, :height => 50 do
   @label = para "waiting for a keypress..."
   keypress do |key|
     @label.replace  case key
-                    when :f1 
-                       "The F1 key was pressed"
-                       
-                    when :f2
-                      
-                      ask_open_file
-                      "Opened file dialog"
-                    else
-                      "the #{key.inspect} key was pressed"
+                      when :f1 
+                        "The F1 key was pressed"  
+                      when :f2
+                        ask_open_file
+                        "Opened file dialog"
+                      else
+                        "the #{key.inspect} key was pressed"
                   end
   end
   
